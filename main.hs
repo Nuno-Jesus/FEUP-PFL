01adcb5d37c7ea77e38333567e1b5faf3d9cd78d
Nuno Jesus <up201905477@edu.fe.up.pt>
2022-10-16 17:06:18 +0100

    Normalize functionality probably done.

--- main.hs
+++ main.hs
@@ -19,38 +19,27 @@ type Polynome = [Monome]
 
 	zipWith sumMonome [(3, [('x', 1)]), (2, [('y', 1)])] [(4, [('x', 1)])]
 
-	
-
 	4xy + 0x + 3z
 	[(4, [('x', 1), ('y', 1)]), (0, [('x', 1)]), (3, [('z', 1)])]
 
     4*x^2 + 3*x + 1*x^2 + y + 0*z 
     [(4,[('x',2)]), (3,[('x',1)]), (1, [('x',2)]), (1,[('y',1)]), (0,[('z',1)])] 
+
+	foldr sumMonome (0, [(' ', 0)]) [(4,[('x',2)]),(1,[('x',2)])] = (5,[('x',2)])
  -}
 
 cleanZeros :: Polynome -> Polynome
-cleanZeros [] = []
-cleanZeros (x:xs)
-  | fst x == 0 = cleanZeros xs
-  | otherwise = x : cleanZeros xs
-
-groupMonomes :: Polynome -> [Polynome]
-groupMonomes p = groupBy sameLiteral (cleanZeros p)
+cleanZeros = filter (\m -> fst m /= 0) 
 
 sameLiteral :: Monome -> Monome -> Bool
 sameLiteral m1 m2 = snd m1 == snd m2
 
+groupMonomes :: Polynome -> [Polynome]
+groupMonomes p = groupBy sameLiteral (sortOn snd (cleanZeros p))
 
-{- sumMonome :: Monome -> Monome -> [Monome]
-sumMonome m1 m2 = 
-	if snd m1 == snd m2 
-		then [(fst m1 + fst m2, snd m1)]
-		else [m1, m2] -}
-
-{- 
-normalizeHelper :: Polynome -> Polynome -> Polynome
-normalizeHelper [] = []
-normalizeHelper (x:xs) acc =  []
+sumAll :: Polynome -> Monome
+sumAll lst = (sum (map fst lst), snd (head lst))
 
 normalize :: Polynome -> Polynome
-normalize p = normalizeHelper (cleanZeros p) -}
+normalize p = [sumAll m | m <- groupMonomes p]
+
