d88fea8bc90220f9fa84fba7b673a34f10a99c37
Nuno Jesus <up201905477@edu.fe.up.pt>
2022-10-18 10:20:28 +0100

    Normalize function done.

--- main.hs
+++ main.hs
@@ -7,35 +7,33 @@ type Polynome = [Monome]
 {- 
 	3x + 2y + 4x
 	[(3, [('x', 1)]), (2, [('y', 1)]), (4, [('x', 1)])]
-
-	[] [(3, [('x', 1)]), (2, [('y', 1)]), (4, [('x', 1)])]
-	[(3, [('x', 1)])] [(2, [('y', 1)]), (4, [('x', 1)])]
-	[(3, [('x', 1)]), (2, [('y', 1)])] [(4, [('x', 1)])]
-
-	sumMonome :: Monome -> Monome -> [Monome]
-	sumMonome m1 m2 = 
-		if snd m1 == snd m2 = (fst m1 + fst m2, snd m1)
-		else = [m1, m2]
-
-	zipWith sumMonome [(3, [('x', 1)]), (2, [('y', 1)])] [(4, [('x', 1)])]
+	
+	3xy + 2yx + 4x
+	[(3, [('x', 1), ('y', 1)]), (2, [('y', 1), ('x', 1)]), (4, [('x', 1)])]
 
 	4xy + 0x + 3z
 	[(4, [('x', 1), ('y', 1)]), (0, [('x', 1)]), (3, [('z', 1)])]
 
-    4*x^2 + 3*x + 1*x^2 + y + 0*z 
-    [(4,[('x',2)]), (3,[('x',1)]), (1, [('x',2)]), (1,[('y',1)]), (0,[('z',1)])] 
+	4xy + 3yx
+	[(4, [('x', 1), ('y', 1)]), (3, [('y', 1), ('x', 1)])]
+
+    4*x^2y^3 + 3*x + 1*y^3x^2 + y + 0*z 
+    [(4,[('x',2), ('y', 3)]), (3,[('x',1)]), (1, [('y', 3), ('x',2)]), (1,[('y',1)]), (0,[('z',1)])] 
 
 	foldr sumMonome (0, [(' ', 0)]) [(4,[('x',2)]),(1,[('x',2)])] = (5,[('x',2)])
  -}
 
+sortLiterals :: Polynome -> Polynome
+sortLiterals p = [(fst x, sort (snd x)) | x <- p]
+
 cleanZeros :: Polynome -> Polynome
-cleanZeros = filter (\m -> fst m /= 0) 
+cleanZeros = filter (\m -> fst m /= 0)
 
 sameLiteral :: Monome -> Monome -> Bool
 sameLiteral m1 m2 = snd m1 == snd m2
 
 groupMonomes :: Polynome -> [Polynome]
-groupMonomes p = groupBy sameLiteral (sortOn snd (cleanZeros p))
+groupMonomes p = groupBy sameLiteral (sortOn snd (sortLiterals(cleanZeros p)))
 
 sumAll :: Polynome -> Monome
 sumAll lst = (sum (map fst lst), snd (head lst))
