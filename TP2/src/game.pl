12325b00babd5aaf52d480f345ea8d229d172c32
Vitor Cavaleiro <vcavaleiro75@gmail.com>
2022-12-29 03:32:40 +0000

    Calculated valid moves

--- TP2/src/game.pl
+++ TP2/src/game.pl
@@ -1,22 +1,90 @@
 :-use_module(library(lists)).
 
 :-consult('board.pl').
+:-consult('tools.pl').
 
 % initialize game
 % init_game(-GameState)
-init_game(gameState(NewBoard, player(0, 8, 'R'), player(1, 8, 'B'))) :-
+init_game(gameState(NewBoard, player(0, 'R', 8), player(1, 'B', 8), 1)) :-
 	init_board(NewBoard).
 	
+% get_piles(+Board, -Piles)
 get_piles([], []).
 get_piles([H|T], Piles) :-
-	findall(X-Y, (length([H|T], X), nth1(Y, H, [_|_])) ,Piles1),
+	findall(X-Y, (length(T, X), nth0(Y, H, [_|_])) ,Piles1),
 	get_piles(T, Piles2),
-	append(Piles1, Piles2, Piles).	
+	append(Piles1, Piles2, Piles).
+	
+% get_paths(+Board, +Position, -Path)
+get_path(Board, X-Y, Path):-
+	at(Board, X, Y, Elem),
+	length(Elem, PileSize),
+	PileSize1 is PileSize+1,
+	length(Board, Limit),
+	dfs(X-Y, PileSize1, Limit, Path).
+	
+dfs(Position, PileSize, Limit, Path) :-
+	dfs1(Position, PileSize, Limit, [Position], ThePath),
+	reverse(ThePath,Path).
 
-cycle(gameState(Board, _, _)) :-
-	display_board(Board),
+dfs1(_, 0, _, Path, Path).
+dfs1(S, PileSize, Limit, SoFar, Path) :-
+	PileSize > 0,
+	PileSize1 is PileSize-1,
+	arc(S,S2, Limit),
+	\+(member(S2,SoFar)),
+	dfs1(S2, PileSize1, Limit, [S2|SoFar], Path).
+	
+arc(X-Y, X1-Y1, Limit):-
+	(X1 is X+0, Y1 is Y+1, X1 < Limit, X1 >= 0, Y1 < Limit, Y1 >= 0 );
+	(X1 is X+0, Y1 is Y-1, X1 < Limit, X1 >= 0, Y1 < Limit, Y1 >= 0 );
+	(X1 is X+1, Y1 is Y+0, X1 < Limit, X1 >= 0, Y1 < Limit, Y1 >= 0 );
+	(X1 is X-1, Y1 is Y+0, X1 < Limit, X1 >= 0, Y1 < Limit, Y1 >= 0 ).
+
+% next_turn(+Last, -Next)
+% next_turn(1, 0).
+% next_turn(0, 1).
+
+% valid_moves(+Board, -List)
+valid_moves(Board, List):-
 	get_piles(Board, Piles),
-	write(Piles).
+	valid_moves_aux(Board, Piles, List).
+
+% valid_moves_aux(+Board, +Piles, -List)
+valid_moves_aux(_, [], []).
+valid_moves_aux(Board, [H|T], List):-
+	findall(Path, (get_path(Board, H, Path)), List1),
+	valid_moves_aux(Board, T, List2),
+	append(List1, List2, List).
+
+% evaluate_move(Board, +Move)
+% evaluate_move(Move):-	
+%	valid_moves(Board, List),
+%	member(Move, List).
+	
+% move(+GameState, +Move, -NewGameState)
+%move(GameState, Move, NewGameState):-
+	
+% push_piece(+Cell, +Piece, -NewCell)
+%push_piece(Cell, Piece, NewCell):-
+
+% walk(+Board, +Path, +Stack, -NewBoard)
+%walk(+Board, +Path, +Stack, -NewBoard)
+
+%cycle(gameState(Board, Player1, Player2, Turn)):-
+	% check winning condition
+
+cycle(gameState(Board, Player1, Player2, Turn)) :-
+	display_game(gameState(Board, Player1, Player2, Turn)),
+	valid_moves(Board, List),
+	get_piles(Board, Piles),
+	write(Piles),
+	nl,
+	write(List).
+%	read_move(Move),
+%	evaluate_move(Board, Move),
+%	move(gameState(Board, Player1, Player2, Turn), Move, NewGameState),
+%	cycle(NewGameState).
 
 
 	
--- TP2/src/tools.pl
+++ TP2/src/tools.pl
@@ -1,7 +1,20 @@
 % replaces Element of old list at index
-% replace(+Old, +Index, +Ele, -New)
-replace([_|T], 0, X, [X|T]).
-replace([H|T], I, X, [H|R]):- 
+% replace_list(+Old, +Index, +Ele, -New)
+replace_list([_|T], 0, X, [X|T]).
+replace_list([H|T], I, X, [H|R]):- 
     I > 0, 
     I1 is I-1, 
-    replace(T, I1, X, R).
+    replace_list(T, I1, X, R).
+
+% replace_matrix(+Old, +IndX, +IndY, +Ele, -New)	
+replace_matrix([List|T], IndX, 0, Ele, [NewList|T]):-
+	replace_list(List, IndX, Ele, NewList).
+replace_matrix([List|T], IndX, IndY, Ele, [List|R]):-
+	IndY > 0,
+	IndY1 is IndY-1,
+	replace_matrix(T, IndX, IndY1, Ele, R).
+	
+% at(+Matrix, +IndX, +IndY, -Val)
+at(Matrix, IndX, IndY, Val) :-
+	nth0(IndX, Matrix, IndX1), 
+	nth0(IndY, IndX1, Val).
